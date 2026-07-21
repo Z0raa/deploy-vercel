ac307e7cf4f681366c610c82e2d2a97e82faa1ea
Avisa deva <avisadeva@students.amikom.ac.id>
2026-07-21 16:27:38 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ class Config:
     """
 
     # Konfigurasi Database MySQL
-    HOST = "localhost"
+    HOST = "a93mx3.h.filess.io"
     PORT = 3306
-    USER = "root"
-    PASSWORD = ""
-    DATABASE = "db_akademik"
+    USER = "db_akademik_quicksonso"
+    PASSWORD = "73233a9cff92bcc133503e840f3fdcf4321a9bbf"
+    DATABASE = "db_akademik_quicksonso"
 
     # Secret Key Flask
     SECRET_KEY = "praktikum-flask-2026"
